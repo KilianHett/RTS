aa31df474fac99943f7a37dfb7dc7e2a8efd8376
Kilian Hett <kilianhett@hotmail.fr>
2014-08-29 14:43:27 +0200

    creation dune lib pour la reconnaissance de forme

---
+++ reptest/cam/reconition.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+import time
+import picamera
+import numpy
+import matplotlib.pyplot as plt
+
+
+class reconition:
+	def __init__(self):
+		return 0;
+	def seuil(self, im, max, min):
+		return im;
+	def houghCircle(self, im):
+		return im;
+	def houghLine(self, im):
+		return im;
