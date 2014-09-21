9f9babc234ad74d897d4fe4e54bebff9be319be1
KilianHett <kilian.hett@etu.unistra.fr>
2014-09-21 17:34:41 +0000

    Ajout fonction configureImpuls libservo.Servo

--- lib/libservo.py
+++ lib/libservo.py
@@ -6,34 +6,34 @@ import time
 import math
 import libport
 
-
-COEF=10**(-4);
 class Servo:
-	_DeadBand=10**(-4);
+	_Unit=10**(-4);
 	_Timp=0;
-	_Tc=15*_DeadBand;
-	_T=25*_DeadBand;
+	_Tmin=10.*_Unit;
+	_Tc=15.*_Unit;
+	_Tmax=20.*_Unit;
 	def __init__(self, port):
 		self._port=port;
 		self._status=False;
-		self.bornes(-180, 180);
+		self.bornes(-90, 90);
 	def bornes(self, bmin, bmax):
 		self._min=bmin;
 		self._max=bmax;
 	def __routine__(self):
-		global COEF;
 		while (self._status):
-			if (self._Timp+self._Tc>=0):
+			if (self._Timp+self._Tc>=self._Tmin and 
+				self._Timp+self._Tc<=self._Tmax):
 				Teff=self._Tc+self._Timp;
 			self._port.write(1);
 			time.sleep(Teff);
 			self._port.write(0);
-			time.sleep(self._T-Teff);
+			time.sleep(self._Tmax-Teff);
 		print "Fin";
-	def configure_servo(self, imin, imax, db):
+	def configureImpulse(self, imin, imax, db):
 		self._Tc=(imin+imax)/2.;
-		self._T=imax;
-		self_DeadBand=db;
+		self._Tmax=imax;
+		self._Tmin=imin;
+		self_Unit=db;
 	def start(self):
 		self._status=True;
 		self._daemon=threading.Thread(None, self.__routine__, self);
@@ -44,7 +44,7 @@ class Servo:
 		self._daemon.join();
 	def rotate(self, teta):
 		assert teta>=self._min and teta<=self._max, "Bad angle";
-		self._Timp=(10.*(teta/180.))*self._DeadBand;
+		self._Timp=(self._Tmax-self._Tmin)*(teta/90.);
 		return self._Timp;
 
 
